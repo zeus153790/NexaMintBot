d9814cd95e0ffee2b6a2808eb42b0f73489df5d8
zeus153790 <kk1236334@gmail.com>
2025-07-17 19:21:38 +0630

    main.py

--- main.py
+++ main.py
@@ -310,11 +310,5 @@ def main():
 
     app.run_polling()
 
-import os
-
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8080))
-    )
-
+    main()
