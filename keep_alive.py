f81ab07bbede665de2dd7ea53bdb7d71b99c34a7
zeus153790 <kk1236334@gmail.com>
2025-07-17 13:27:27 +0630

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,3 @@
-# keep_alive.py
 from flask import Flask
 from threading import Thread
 
@@ -6,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "NexaMint Bot is running!"
+    return "NexaMint Bot is running! 🚀"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
